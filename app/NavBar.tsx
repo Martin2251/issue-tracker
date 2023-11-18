765545b201744d0ca5e6cc08083b3775d290e38f
Martin Lowe <martinlowe673@gmail.com>
2023-11-18 14:27:54 +0000

    styling active link with classnames

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,17 +1,28 @@
+"use client"
 import React from 'react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import {AiFillBug} from "react-icons/ai"
+import classNames from 'classnames'
 
 const NavBar = () => {
+
+    const currentPath = usePathname();
+
     const links = [
         {label:"Dashboard", href: "/"},
         {label:"Issues", href: "/issues"},
     ]
+
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
         <Link href="/"><AiFillBug  /></Link>
         <ul className='flex space-x-6'>
-            {links.map(link =>  <Link key={link.href} className='text-zinc-500 hover:text-zinc-800 transition-colors' href={link.href}>{link.label}</Link>)}
+            {links.map(link =>  <Link key={link.href} className={classNames({ 
+                "text-zinc-900" : link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors" :true
+            })} href={link.href}>{link.label}</Link>)}
         </ul>
     </nav>
   )
---
+++ app/issues/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IssuesPage = () => {
+  return (
+    <div>Issues</div>
+  )
+}
+
+export default IssuesPage
